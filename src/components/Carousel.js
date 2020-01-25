ec63b1e12d1cc9931b24a436fe9c42dc51697c3f
Dennis Osipov <osdenn@gmail.com>
2020-01-25 21:41:23 +0100

    moved CarouselSlide into Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
 import Slider from 'infinite-react-carousel';
 import * as PropTypes from "prop-types";
-import CarouselSlide from './CarouselSlide';
 import styles from './Carousel.module.scss'
+import BackgroundImage from "gatsby-background-image";
 
 class Carousel extends Component {
   render() {
-    let {className, items} = this.props;
+    let { className, items} = this.props;
     const settings = {
-      adaptiveHeight: true,
+      adaptiveHeight: false,
       arrowsBlock: false,
       className: className,
       duration: 100,
       wheel: true
     };
     return (
-      <Slider {...settings}>
-        {
-            items.map(props => CarouselSlide(props))
-        }
-      </Slider>
+        <Slider {...settings}>{ items.map(item => (
+            <BackgroundImage key={item.image.id} className={styles.slide} fluid={item.image.childImageSharp.fluid}>
+                { item.title === undefined ? '' : (
+                    <div className={styles.descriptionPanel}>
+                        <h3>{item.index + 1} - {item.title}</h3>
+                    </div>
+                ) }
+            </BackgroundImage>
+        )) }</Slider>
     );
   }
 }
