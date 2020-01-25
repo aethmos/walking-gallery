8b55bf3f994870a8508711e6257ca1e934fe1aff
Dennis Osipov <osdenn@gmail.com>
2020-01-25 21:42:58 +0100

    replaced with single query and CategorySlider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,52 @@
 import React from 'react';
 import Layout from '../components/layout';
-import {graphql, Link} from "gatsby";
-import BackgroundImage from 'gatsby-background-image'
-
-import styles from '../components/CarouselSlide.module.scss'
-let maxZIndex = 0;
+import CategoryCarousel from "../components/CategoryCarousel";
+import {graphql} from "gatsby";
 
 const IndexPage = ({data}) => (
     <Layout>
-        <div className={styles.header}><h1>{data.collections.totalCount} Collections</h1></div>
-        <ul>
-            {data.collections.nodes.map(collection => {
-                let images = data.images.group.filter(group => group.edges[0].node.relativeDirectory === collection.directory)[0];
-                console.log(images.edges);
-                let coverImage = images.edges[0].node.childImageSharp.fluid;
-
-                return (
-                    <li key={collection.id} style={{zIndex: maxZIndex--}}>
-                        <Link to={'/' + collection.slug + '/'}>
-                            <BackgroundImage Tag={'div'} className={styles.slide} fluid={coverImage} style={{zIndex: maxZIndex--}}>
-                                <div className={styles.descriptionPanel}>
-                                    <h3>{collection.title}</h3>
-                                    <span>{images.totalCount} images</span>
-                                    <span>{collection.date}</span>
-                                </div>
-                            </BackgroundImage>
-                        </Link>
-                    </li>
-                )
-            })}
-        </ul>
+        <CategoryCarousel categories={data.categories.nodes} thumbnails={data.thumbnails.edges.map(edge => edge.node)}/>
     </Layout>
 );
 
 export const query = graphql`
-    query CollectionsQuery {
-        collections: allCollectionsJson(
-                sort: {order: DESC, fields: date}, 
-#                filter: {directory: {regex: "/2020/"}}
-        ) {
-            nodes {
-                id
-                slug
-                title
-                directory
-                date(fromNow: true)
-            }
-            totalCount
-        }
-
-        images: allFile(
-                filter: {extension: {regex: "/jpg|png/"}}, 
-                sort: {order: ASC, fields: name}
-        ) {
-            group(field: relativeDirectory, limit: 1) {
-              edges {
-                node {
-                  relativePath
-                  absolutePath
-                  relativeDirectory
-                  name
-                  id
-                  extension
-                  childImageSharp {
-                    fluid(
-                      cropFocus: ATTENTION, 
-                      maxHeight: 1920
-                    ) {
-                      src
+            query CategoriesAndThumbnails{
+                categories: allCollectionsJson {
+                    nodes {
+                        id
+                        slug
+                        title
+                        relativeDirectory
+                        date(fromNow: true)
+                        startIdx
                     }
-                  }
+                    totalCount
                 }
-              }
-              totalCount
+                thumbnails: allFile(
+                filter: {
+                    extension: {regex: "/jpg|png/"}, 
+                    name: {regex: "/thumb.*/i"}
+                }, sort: {
+                    order: ASC, 
+                    fields: name
+                }) {
+                    edges {
+                        node {
+                            id
+                            name
+                            extension
+                            relativeDirectory
+                            childImageSharp {
+                                fluid(maxHeight: 1920, quality: 75, cropFocus: ATTENTION) {
+                                    aspectRatio
+                                    sizes
+                                    src
+                                }
+                            }
+                        }
+                    }
+                }
             }
-          }
-    }
-`;
+        `;
 
 export default IndexPage;
