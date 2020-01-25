1dd192ee306fd8c727a0767a9da61e0102a16ae7
Dennis Osipov <osdenn@gmail.com>
2020-01-26 00:36:44 +0100

    moved carousel into page template

--- src/components/AlbumTemplate.js
+++ src/components/AlbumTemplate.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Carousel from "./Carousel";
-import {graphql} from "gatsby";
+import {graphql, Link} from "gatsby";
+import styles from "./Carousel.module.scss";
+import Slider from "infinite-react-carousel";
+import BackgroundImage from "gatsby-background-image";
 
 const AlbumTemplate = ({data: {category, images}}) => {
     images = images.edges.map(edge => {
         let image = edge.node;
         image.category = category;
+        image.link = '/';
         return image;
     });
     category.totalImages = images.length;
+
+    const settings = {
+      adaptiveHeight: false,
+      arrowsBlock: false,
+      className: styles.carousel,
+      duration: 100,
+      wheel: true,
+      initialSlide: category.thumbIdx
+    };
     return (
         <Layout>
-            <Carousel images={images} initialSlide={}{category.thumbIdx}/>
+            <Slider {...settings}>{ images.map((image, index) => (
+                <Link to={image.link ? image.link : '#'}>
+                    <BackgroundImage key={image.id} className={styles.slide} fluid={image.childImageSharp.fluid}>
+                        { image.category?.title === undefined ? '' : (
+                            <div className={styles.descriptionPanel}>
+                                <h3>{index + 1} - {image.category.title}</h3>
+                            </div>
+                        ) }
+                    </BackgroundImage>
+                </Link>
+            )) }</Slider>
         </Layout>
     );
 };
 
 export const query = graphql`
-            query CategoriesAndThumbnails{
+            query CategoryAndImages($categoryId: String, $path: String) {
                 category: categoryJson(id: { eq: $categoryId}) {
                     id
                     title
                     relativeDirectory
                     date(fromNow: true)
                     thumbIdx
-                }
+                } 
                 images: allFile(
                 filter: {
                     extension: {regex: "/jpg|png/"}, 
@@ -36,7 +58,7 @@ export const query = graphql`
                 })
                 {
                     edges {
-                        nodes {
+                        node {
                             id
                             name
                             extension
@@ -46,6 +68,7 @@ export const query = graphql`
                                     aspectRatio
                                     sizes
                                     src
+                                    srcSet
                                 }
                             }
                         }
