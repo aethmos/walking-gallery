9b7639b2631bf0bbe876740a26216e48ea68f7b5
Dennis Osipov <osdenn@gmail.com>
2020-01-25 23:30:57 +0100

    create pages from category.json

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,14 +4,36 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
+exports.onCreateNode = ({node, actions, getNode}) => {
     // if (node.internal.mediaType === 'image/jpeg')
     console.log(node);
+};
 
-    if (node.internal.type === 'CategoryJson') {
-        let { createPage } = actions;
-
+exports.createPages = async ({graphql, actions, reporter}) => {
+    const {createPage} = actions;
+    const data = await graphql(`
+    query {
+      categories: allCategoryJson {
+          nodes {
+            id
+            relativeDirectory
+          }
+      }
     }
+  `);
+    if (data.errors) {
+        reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+    }
+    const categories = data.categories.nodes;
+    categories.map((value, index) => {
+        createPage({
+            path: value.relativeDirectory,
+            component: path.resolve(`./src/components/AlbumTemplate.js`),
+            context: {
+                categoryId: value.id,
+                path: value.relativeDirectory,
+
+            },
+        })
+    });
 };
