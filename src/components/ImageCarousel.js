3a503363e407f871818f258b9638fa97cab8a413
Dennis Osipov <osdenn@gmail.com>
2020-01-25 23:13:16 +0100

    simplified and renamed to ImageCarousel

---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Slider from 'infinite-react-carousel';
+import * as PropTypes from "prop-types";
+import styles from './Carousel.module.scss'
+import BackgroundImage from "gatsby-background-image";
+
+class ImageCarousel extends Component {
+  render() {
+    let { images} = this.props;
+    const settings = {
+      adaptiveHeight: false,
+      arrowsBlock: false,
+      className: styles.carousel,
+      duration: 100,
+      wheel: true
+    };
+    return (
+        <Slider {...settings}>{ images.map((item, index) => (
+            <BackgroundImage key={item.id} className={styles.slide} fluid={item.childImageSharp.fluid}>
+                { item.category.title === undefined ? '' : (
+                    <div className={styles.descriptionPanel}>
+                        <h3>{index + 1} - {item.category.title}</h3>
+                    </div>
+                ) }
+            </BackgroundImage>
+        )) }</Slider>
+    );
+  }
+}
+
+ImageCarousel.propTypes = {
+  images: PropTypes.array.isRequired
+};
+
+export default ImageCarousel;
