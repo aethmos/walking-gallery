0d3d823a0c4b597d772ac90596275bbc45e110dc
Dennis Osipov <osdenn@gmail.com>
2020-01-27 04:13:39 +0100

    pass accelerometer values to slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,6 +4,7 @@ import Slide from "./Slide";
 import Icon from "@iconify/react";
 import leftArrow from "@iconify/icons-mdi-light/chevron-left";
 import rightArrow from "@iconify/icons-mdi-light/chevron-right";
+import Accelerometer from "./Accelerometer";
 
 const Slider = ({
         className = styles.slider,
@@ -14,13 +15,17 @@ const Slider = ({
     const [currentIndex, setCurrentIndex] = useState(initialIndex || 0);
 
     return (
-        <div className={className}>
-            <div className={`${styles.arrows} ${styles.left}`}><Icon icon={leftArrow}/></div>
-            <div className={`${styles.arrows} ${styles.right}`}><Icon icon={rightArrow}/></div>
-            {sections.map((content, index) => {
-                return <Slide index={index} totalSlides={sections.length} content={content} active={index === currentIndex}/>
-            })}
-        </div>
+        <Accelerometer>
+            {(acceleration, rotation) => (
+                <div className={className}>
+                    <div className={`${styles.arrows} ${styles.left}`}><Icon icon={leftArrow}/></div>
+                    <div className={`${styles.arrows} ${styles.right}`}><Icon icon={rightArrow}/></div>
+                    {sections.map((content, index) => {
+                        return <Slide index={index} currentIndex={currentIndex} totalSlides={sections.length} content={content}/>
+                    })}
+                </div>
+            )}
+        </Accelerometer>
     )
 };
 
