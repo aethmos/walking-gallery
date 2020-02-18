39784b6cb62b1e0c678a1ec437355c90b584c578
Dennis Osipov <osdenn@gmail.com>
2020-02-18 01:40:40 +0100

    adjusted stepping thresholds

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,8 +7,8 @@ import rightArrow from "@iconify/icons-mdi-light/chevron-right";
 import Accelerometer from "./Accelerometer";
 import {wrap} from "@popmotion/popcorn";
 
-const stepInThreshold = 10;
-const stepOutThreshold = 5;
+const stepInThreshold = 15;
+const stepOutThreshold = 20;
 const stepInOutBufferMax = 15;
 const stepInOutBufferMin = 10;
 const stepInOutCooldownMilliseconds = 3000;
