d741b4bbe8db0dad816bfa76bffbc9493204af0f
Dennis Osipov <osdenn@gmail.com>
2020-02-20 01:58:07 +0100

    cleaned up imports

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React from "react";
 import styles from "./header.module.scss";
 import {Link} from "gatsby";
 import {Icon} from "@iconify/react";
